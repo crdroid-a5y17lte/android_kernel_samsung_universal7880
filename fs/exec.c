a393d56ce06b75deda3dfe45eed4284eedca154c
cyberc3dr <geo3gamer@gmail.com>
2025-11-18 16:23:46 +0300

    scope-minimized manual hooks v1.6

--- fs/exec.c
+++ fs/exec.c
@@ -1607,6 +1607,12 @@ static int exec_binprm(struct linux_binprm *bprm)
 /*
  * sys_execve() executes a new program.
  */
+#ifdef CONFIG_KSU
+__attribute__((hot))
+extern int ksu_handle_execveat(int *fd,
+			struct filename **filename_ptr,
+			void *argv, void *envp, int *flags);
+#endif
 static int do_execve_common(struct filename *filename,
 				struct user_arg_ptr argv,
 				struct user_arg_ptr envp)
@@ -1618,6 +1624,10 @@ static int do_execve_common(struct filename *filename,
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
+
+#ifdef CONFIG_KSU
+	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+#endif	
 
 	/*
 	 * We move the actual failure in case of RLIMIT_NPROC excess from
@@ -1741,6 +1751,7 @@ int do_execve(struct filename *filename,
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
+	
 	return do_execve_common(filename, argv, envp);
 }
 
