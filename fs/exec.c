3eb257a3bbd4370d2f01520d0aa28a34aace2157
cyberc3dr <47525358+cyberc3dr@users.noreply.github.com>
2026-08-10 10:14:45 +0000

    do_execve_common: mark as used

--- fs/exec.c
+++ fs/exec.c
@@ -1607,6 +1607,7 @@ static int exec_binprm(struct linux_binprm *bprm)
 /*
  * sys_execve() executes a new program.
  */
+__attribute__((used))
 static int do_execve_common(struct filename *filename,
 				struct user_arg_ptr argv,
 				struct user_arg_ptr envp)
