eddcb29d91e58731cfc65ec51b12117a37f156be
cyberc3dr <47525358+cyberc3dr@users.noreply.github.com>
2026-08-10 14:13:11 +0000

    try this

--- fs/exec.c
+++ fs/exec.c
@@ -1607,7 +1607,7 @@ static int exec_binprm(struct linux_binprm *bprm)
 /*
  * sys_execve() executes a new program.
  */
-__attribute__((used, noipa))
+__attribute__((used)) asmlinkage
 static int do_execve_common(struct filename *filename,
 				struct user_arg_ptr argv,
 				struct user_arg_ptr envp)
