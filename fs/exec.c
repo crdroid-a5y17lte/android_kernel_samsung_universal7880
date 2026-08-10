e1d446589319016f368e8718c7a900a05ad704ed
cyberc3dr <47525358+cyberc3dr@users.noreply.github.com>
2026-08-10 14:51:00 +0000

    __attribute__((used))

--- fs/exec.c
+++ fs/exec.c
@@ -1615,6 +1615,7 @@ static int exec_binprm(struct linux_binprm *bprm)
 /*
  * sys_execve() executes a new program.
  */
+__attribute__((used))
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
